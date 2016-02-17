8966f27a2a60d844ad8da8b3e4f4dbbd9ee41776
srikantviswanath <srikantviswanath8@gmail.com>
2016-02-17 00:35:59 -0500

    Boiler plate UI code containing FiterToDumpVC, ListViewVC, ListViewCell and using a lis of hardcoded dictionaries as aplace holder to populate ListViewVC

---
+++ keychainEditrUI/keychainEditrUI/ListViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListViewCell.swift
+//  keychainEditrUI
+//
+//  Created by Srikant Viswanath on 2/16/16.
+//  Copyright © 2016 Ghutle. All rights reserved.
+//
+
+import UIKit
+
+class ListViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
